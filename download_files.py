0ff5c588129f195430a806a5aeb829dc055b8825
D0men1c0 <82759768+D0men1c0@users.noreply.github.com>
2024-07-08 00:37:37 +0200

    Update download_files.py

--- download_files.py
+++ download_files.py
@@ -1,6 +1,10 @@
-import gdown
 import os
+import gdown
+import logging
 from tqdm import tqdm
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def download_file_from_google_drive(file_id, destination):
     # Construct the download URL for the file from its ID
@@ -18,12 +22,22 @@ files_to_download = [
 
 # Download each file listed in files_to_download
 for file in tqdm(files_to_download, desc="Downloading files"):
+    while True:
+        should_download = input(f"Do you want to download the file to {file['destination']}? (yes/no or y/n): ").strip().lower()
+        
+        if should_download in ['yes', 'y']:
+            # Create the directory structure if it doesn't exist
+            os.makedirs(os.path.dirname(file["destination"]), exist_ok=True)
 
-    # Create the directory structure if it doesn't exist
-    os.makedirs(os.path.dirname(file["destination"]), exist_ok=True)
-
-    # Download the file using the defined function
-    download_file_from_google_drive(file["file_id"], file["destination"])
-
-    # Print a confirmation message after successful download
-    print(f"Download completed. The file has been saved to: {file['destination']}")
+            # Download the file using the defined function
+            try:
+                download_file_from_google_drive(file["file_id"], file["destination"])
+                logging.info(f"Download completed. The file has been saved to: {file['destination']}")
+            except Exception as e:
+                logging.error(f"An error occurred while downloading the file: {e}")
+            break
+        elif should_download in ['no', 'n']:
+            logging.info(f"Skipped downloading the file to: {file['destination']}")
+            break
+        else:
+            logging.warning("Invalid input. Please enter 'yes', 'no', 'y', or 'n'.")
