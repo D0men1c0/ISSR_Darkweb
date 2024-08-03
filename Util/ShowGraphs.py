88832ebf0bd7dd86764df5b8b2c33ffd38f82d3c
D0men1c0 <82759768+D0men1c0@users.noreply.github.com>
2024-08-03 16:25:55 +0200

    Update ShowGraphs.py

--- Util/ShowGraphs.py
+++ Util/ShowGraphs.py
@@ -3,6 +3,8 @@ from bertopic import BERTopic
 import pandas as pd
 import seaborn as sns
 import math
+import re
+import numpy as np
 from wordcloud import WordCloud
 from collections import defaultdict
 import networkx as nx
@@ -10,12 +12,12 @@ from typing import Union
 from tqdm import tqdm
 
 
-def plot_topic_distribution(df: pd.DataFrame) -> None:
+def plot_topic_distribution(df: pd.DataFrame, figsize: tuple = (8, 6)) -> None:
     """
     Create a horizontal bar plot for topic distribution based on counts.
     :param df: DataFrame containing 'Count' and 'Custom_Name_GenAI' columns.
     """
-    plt.figure(figsize=(8, 6))
+    plt.figure(figsize=figsize)
     sns.barplot(x='Count', y='Custom_Name_GenAI', data=df, orient='h',
                 order=df.sort_values('Count', ascending=False)['Custom_Name_GenAI'])
     
@@ -25,34 +27,61 @@ def plot_topic_distribution(df: pd.DataFrame) -> None:
     plt.tight_layout()
     plt.show()
 
-def plot_topic_percentage_distribution(df: pd.DataFrame) -> None:
+def clean_probability_string(prob_string: str) -> list:
+    """
+    Clean and convert a probability string into a list of numbers.
+    :param prob_string: String containing probabilities.
+    :return: List of probabilities as floats.
+    """
+    # Remove unwanted characters such as '\n' and add commas between numbers
+    cleaned_str = re.sub(r'\s+', ',', prob_string.strip())
+
+    # Add a comma between numbers where it's missing
+    cleaned_str = re.sub(r'(\d)\s+(\d)', r'\1,\2', cleaned_str)
+
+    return eval(f'[{cleaned_str}]')
+
+def extract_max_probabilities(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Extract the maximum probability for each record in the DataFrame.
+    :param df: DataFrame containing 'Probability' column with probability strings.
+    :return: DataFrame with an additional 'Max_Probability' column.
+    """
+    # Clean and convert the probability strings into lists of numbers
+    df['Probability'] = df['Probability'].apply(clean_probability_string)
+
+    # Calculate the maximum probability for each record
+    df['Max_Probability'] = df['Probability'].apply(np.max)
+    return df
+
+def plot_topic_percentage_distribution(df: pd.DataFrame, figsize: tuple = (8, 6)) -> None:
     """
     Create a pie chart for the percentage distribution of topics based on counts.
     :param df: DataFrame containing 'Count' and 'Topic' columns.
     """
-    plt.figure(figsize=(8, 6))
+    plt.figure(figsize=figsize)
     plt.pie(df['Count'], labels=df['Topic'], autopct='%1.1f%%', startangle=140, colors=plt.cm.Paired(range(len(df))))
     
     plt.title('Topic Percentage Distribution')
     plt.tight_layout()
     plt.show()
 
-def plot_avg_prob_or_freq(df: pd.DataFrame, metric: str = 'Probability') -> None:
+def plot_avg_prob_or_freq(df: pd.DataFrame, metric: str = 'Probability', figsize: tuple = (10, 6)) -> None:
     """
     Create a bar plot of average probability or average frequency by topic.
     :param df: DataFrame containing 'Topic' and either 'Probability' or 'Frequency' columns.
     :param metric: Metric to plot; should be 'Probability' or 'Frequency'.
     """
-    if metric not in ['Probability', 'Frequency']:
-        raise ValueError("Metric should be 'Probability' or 'Frequency'")
+    if metric not in ['Probability', 'Frequency', 'Max_Probability']:
+        raise ValueError("Metric should be 'Frequency' or 'Probability' or 'Max_Probability'")
     
     avg_metric = df.groupby('Topic')[metric].mean().reset_index()
     avg_metric = avg_metric.sort_values(metric)
     
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=figsize)
     
     # Use seaborn for 'Probability' or matplotlib for 'Frequency'
-    if metric == 'Probability':
+    if metric == 'Probability' or metric == 'Max_Probability':
         sns.barplot(x='Topic', y=metric, data=avg_metric, order=avg_metric['Topic'])
     else:
         plt.bar(avg_metric['Topic'], avg_metric[metric], color='skyblue')
@@ -64,16 +93,16 @@ def plot_avg_prob_or_freq(df: pd.DataFrame, metric: str = 'Probability') -> None
     plt.tight_layout()
     plt.show()
 
-def plot_boxplot(df: pd.DataFrame, metric: str = 'Probability') -> None:
+def plot_boxplot(df: pd.DataFrame, metric: str = 'Probability', figsize: tuple = (10, 6)) -> None:
     """
     Create a box plot for either frequency or probability by topic.
     :param df: DataFrame with 'Topic' and 'Frequency' or 'Probability'.
     :param metric: 'Frequency' or 'Probability' to plot.
     """
-    if metric not in ['Frequency', 'Probability']:
-        raise ValueError("Metric should be 'Frequency' or 'Probability'")
+    if metric not in ['Probability', 'Frequency', 'Max_Probability']:
+        raise ValueError("Metric should be 'Frequency' or 'Probability' or 'Max_Probability'")
     
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=figsize)
     
     sns.boxplot(data=df, x='Topic', y=metric, palette='Set2')
     plt.title(f'Box Plot of {metric} by Topic')
@@ -83,12 +112,12 @@ def plot_boxplot(df: pd.DataFrame, metric: str = 'Probability') -> None:
     plt.tight_layout()
     plt.show()
 
-def plot_probability_distribution(df: pd.DataFrame) -> None:
+def plot_probability_distribution(df: pd.DataFrame, feature: str = 'Probability', figsize: tuple = (10, 6)) -> None:
     """
     Create a histogram with KDE for the distribution of document probabilities.
     :param df: DataFrame containing a 'Probability' column.
     """
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=figsize)
     sns.histplot(df['Probability'], bins=20, kde=True, color='skyblue')
     
     plt.title('Distribution of Document Probabilities')
@@ -97,7 +126,7 @@ def plot_probability_distribution(df: pd.DataFrame) -> None:
     plt.tight_layout()
     plt.show()
 
-def create_wordclouds(data: Union[BERTopic, pd.DataFrame], num_topics: int, cols: int = 3, is_model: bool = True) -> None:
+def create_wordclouds(data: Union[BERTopic, pd.DataFrame], num_topics: int, cols: int = 3, is_model: bool = True, width: int = 1200, height: int = 500) -> None:
     """
     Create word clouds for each topic based on either a BERTopic model or a DataFrame.
     :param data: BERTopic model or DataFrame containing 'Topic' and 'Document' columns.
@@ -115,7 +144,7 @@ def create_wordclouds(data: Union[BERTopic, pd.DataFrame], num_topics: int, cols
         for i in range(num_topics):
             ax = axes[i]
             text = {word: value for word, value in data.get_topic(i)}
-            wc = WordCloud(background_color="black", max_words=1000, width=1200, height=500)
+            wc = WordCloud(background_color="black", max_words=1000, width=width, height=height)
             wc.generate_from_frequencies(text)
             
             ax.imshow(wc, interpolation="bilinear")
@@ -129,7 +158,7 @@ def create_wordclouds(data: Union[BERTopic, pd.DataFrame], num_topics: int, cols
             if i < len(text_by_topic):
                 ax = axes[i]
                 document_text = text_by_topic.loc[text_by_topic['Topic'] == i, 'Document'].values[0]
-                wc = WordCloud(background_color="black", max_words=1000, width=1200, height=500)
+                wc = WordCloud(background_color="black", max_words=1000, width=width, height=height)
                 wc.generate(document_text)
                 
                 ax.imshow(wc, interpolation="bilinear")
@@ -145,7 +174,7 @@ def create_wordclouds(data: Union[BERTopic, pd.DataFrame], num_topics: int, cols
     plt.tight_layout()
     plt.show()
 
-def create_topic_network_graphs(df: pd.DataFrame, window_size: int = 5, min_occurrences: int = 1, cols: int = 2, max_words: int = 100) -> None:
+def create_topic_network_graphs(df: pd.DataFrame, window_size: int = 5, min_occurrences: int = 1, cols: int = 2, max_words: int = 100, width: int = 24, height: int = 8) -> None:
     """
     Create network graphs for each topic visualizing relationships between topics and documents.
     :param df: DataFrame containing 'Topic', 'Document', and 'BERTopic_Name' columns.
@@ -159,7 +188,7 @@ def create_topic_network_graphs(df: pd.DataFrame, window_size: int = 5, min_occu
     rows = math.ceil(num_topics / cols)
     
     # Increase the figure size for larger graphs
-    fig, axes = plt.subplots(rows, cols, figsize=(24, rows * 8), sharex=True, sharey=True)
+    fig, axes = plt.subplots(rows, cols, figsize=(width, rows * height), sharex=True, sharey=True)
     axes = axes.flatten()
     
     for idx, topic in tqdm(enumerate(topics), total=num_topics, desc='Processing Topics'):
